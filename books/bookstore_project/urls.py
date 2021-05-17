b16a111165fb2e8b123ce79cbe68a0409b585c06
jpaetsch <j.paetsch@live.com>
2021-05-17 15:43:01 -0600

    Chapter 5 - added user registration, login, and logout functionality

--- books/bookstore_project/urls.py
+++ books/bookstore_project/urls.py
@@ -17,6 +17,14 @@ from django.contrib import admin
 from django.urls import path, include # newly added include
 
 urlpatterns = [
+    
+    # Django admin
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')), # newly added
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('accounts/', include('users.urls')),
+    path('', include('pages.urls')),
 ]
--- books/users/tests.py
+++ books/users/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
 
-# Two test cases for confirming that a new user and new superuser can be created
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
+
+# Note that the previous version of Django used in this textbook uses setUp(), but TestCase has been
+# updated and setUpTestData() can be used to allow initial data creation and greatly increase speeds of
+# test suites - look into for potential optimization
+
+# Test cases for confirming that a new user and new superuser can be created
 # To run, use 'python manage.py test' >> if in docker, prefix this command with 'docker-compose exec web'
 class CustomUserTests(TestCase):
 
@@ -32,3 +40,27 @@ class CustomUserTests(TestCase):
         self.assertTrue(super_user.is_active)
         self.assertTrue(super_user.is_staff)
         self.assertTrue(super_user.is_superuser)
+
+
+# Test cases for user signup (login and logout are implemented through Django's built-in
+# auth functionality)
+class SignupPageTests(TestCase):
+    
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+    
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
