6518b7b7ea241cbbeda6f5f8a928d789d27dc485
jpaetsch <j.paetsch@live.com>
2021-05-25 22:10:25 -0600

    Integrated simple environment variable usage as per Chapter 8

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m$0l25uc39crj#nj-*q+en)f(%d4t!1b0)-7c10t4=vcv-l*mf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
