ad6997880858909f293928dc620bfcf1dd1bd6fb
jpaetsch <j.paetsch@live.com>
2021-05-25 21:27:00 -0600

    Set up advanced user registration and extendable social authentication as per Chapter 7

--- books/users/tests.py
+++ books/users/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse, resolve
 
-from .forms import CustomUserCreationForm
-from .views import SignupPageView
+# from .forms import CustomUserCreationForm
+# from .views import SignupPageView
 
 # Note that the previous version of Django used in this textbook uses setUp(), but TestCase has been
 # updated and setUpTestData() can be used to allow initial data creation and greatly increase speeds of
@@ -42,25 +42,48 @@ class CustomUserTests(TestCase):
         self.assertTrue(super_user.is_superuser)
 
 
-# Test cases for user signup (login and logout are implemented through Django's built-in
-# auth functionality)
-class SignupPageTests(TestCase):
-    
+# Test cases for user signup (using django-allauth app functionality)
+class SignupTests(TestCase):
+
+    username = 'newuser'
+    email = 'newuser@email.com'
+
     def setUp(self):
-        url = reverse('signup')
+        url = reverse('account_signup')
         self.response = self.client.get(url)
-    
+
     def test_signup_template(self):
         self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertTemplateUsed(self.response, 'account/signup.html')
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
 
     def test_signup_form(self):
-        form = self.response.context.get('form')
-        self.assertIsInstance(form, CustomUserCreationForm)
-        self.assertContains(self.response, 'csrfmiddlewaretoken')
+        new_user = get_user_model().objects.create_user(self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
 
-    def test_signup_view(self):
-        view = resolve('/accounts/signup/')
-        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
+
+# # Test cases for user signup (login and logout are implemented through Django's built-in
+# # auth functionality)
+# class SignupPageTests(TestCase):
+    
+#     def setUp(self):
+#         url = reverse('signup')
+#         self.response = self.client.get(url)
+    
+#     def test_signup_template(self):
+#         self.assertEqual(self.response.status_code, 200)
+#         self.assertTemplateUsed(self.response, 'signup.html')
+#         self.assertContains(self.response, 'Sign Up')
+#         self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+#     def test_signup_form(self):
+#         form = self.response.context.get('form')
+#         self.assertIsInstance(form, CustomUserCreationForm)
+#         self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+#     def test_signup_view(self):
+#         view = resolve('/accounts/signup/')
+#         self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
